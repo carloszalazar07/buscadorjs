d663899a10df73a2eeb8b9b8c018fd902eaae300
Carlos <zalazaarcarlos@gmail.com>
2019-01-11 01:58:49 -0300

    Hola Que Hace

--- js/buscador.js
+++ js/buscador.js
@@ -1,16 +1,15 @@
-
 $(document).ready(function(){
 	$('#search').keyup(function(){
 		$('#result').html('');
 		var searchField = $('#search').val();
 		var expression = new RegExp(searchField, "i");
 
-		$.getJSON('../data.json', function(data){
+		$.getJSON('data.json', function(data){
 			$.each(data, function(key, value){
 				if (value.name.search(expression) != -1 || value.location.search(
 					expression) != -1 ) 
 				{
-					$('#result').append('<li><h2>'+value.name+'</h2> | <span>'+value.location+'<span></li>');
+					$('#result').append('<li><h4 class="text-center">'+value.name+'</h4><p class="text-center">'+value.location+'<p></li>');
 				}
 			})
 		});
